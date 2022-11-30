19e1022799e0c6f399b75cfcc887583410a01b42
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-11-30 13:09:15 -0500

    update `Main.java`
    #3

--- Assignment1-Swing-Events/src/gui_swing_events/Main.java
+++ Assignment1-Swing-Events/src/gui_swing_events/Main.java
@@ -1,5 +1,9 @@
 package gui_swing_events;
 
+// the package statement
+// the Class file that contains the main() method as the entry-point
+// main() method for just establishing the "SwingEventWindow" class
+
 public class Main {
   
 }
