a9993b828500e00d92c5e510c58cbe5b9b3cbb94
Tristan Loye <73702803+trisl01@users.noreply.github.com>
2022-12-03 15:25:11 -0500

    update delete old method
    #19

--- Assignment1-Swing-Events/src/gui_swing_events/SwingEventWindow.java
+++ Assignment1-Swing-Events/src/gui_swing_events/SwingEventWindow.java
@@ -119,15 +119,6 @@ public class SwingEventWindow extends JFrame implements ActionListener, ItemList
   // Implementing the two required methods (event-handler)
   // for each interface:
   @Override
-  public void Event-Handler method for the radio buttons {
-    if (first radio button for total is selected) then:
-      // set the value of the flag variable "rdoChecked" to 1
-    else if (second radio button for average is selected) then:
-      // set the value of the flag variable "rdoChecked" to 2
-    else if (third radio button for max is selected) then:
-      // set the value of the flag variable "rdoChecked" to 3
-    else then:
-      // set the value of the flag variable "rdoChecked" to 4
   }
 
   @Override
